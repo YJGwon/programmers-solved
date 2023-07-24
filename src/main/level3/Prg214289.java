b72e6bf35f26ded42bb5d89f584f575dd8abb0d3
YJGwon <trulyforky@gmail.com>
2023-07-24 23:53:29 +0900

    enhance: 214289 에어컨(재귀 -> dp)

--- src/main/level3/Prg214289.java
+++ src/main/level3/Prg214289.java
@@ -9,78 +9,57 @@ import java.util.Map;
 // https://school.programmers.co.kr/learn/courses/30/lessons/214289
 class Prg214289 {
 
-    private int outside;
-    private int minT;
-    private int maxT;
-    private int coastDiff;
-    private int coastSame;
-    private int[] onboard;
-
-    // minCoast.get(시간).get(실내온도) = 최소전력
-    private List<Map<Integer, Integer>> minCoast;
+    private int min;
+    private int max;
 
     public int solution(int temperature, int t1, int t2, int a, int b, int[] onboard) {
-        outside = temperature;
-        minT = t1;
-        maxT = t2;
-        coastDiff = a;
-        coastSame = b;
-        this.onboard = onboard;
+        min = t1;
+        max = t2;
 
-        minCoast = new ArrayList<>();
-        for (int i = 0; i < onboard.length; i++) {
-            minCoast.add(new HashMap<>());
+        List<Map<Integer, Integer>> mem = new ArrayList<>(); // get(a).get(b) = a 시점에 b 온도를 만드는 최소비용
+        mem.add(Map.of(temperature, 0));
+
+        for (int i = 1; i < onboard.length; i++) {
+            Map<Integer, Integer> now = new HashMap<>();
+
+            Map<Integer, Integer> prev = mem.get(i - 1);
+            for (int prevTemperature : prev.keySet()) {
+                int prevCoast = prev.get(prevTemperature);
+
+                if (onboard[i] != 1 || isInRange(prevTemperature - 1)) {
+                    // 온도 1도 낮추기
+                    int coast = temperature < prevTemperature ? prevCoast : prevCoast + a;
+                    updateMinCoast(prevTemperature - 1, coast, now);
+                }
+
+                if (onboard[i] != 1 || isInRange(prevTemperature + 1)) {
+                    // 온도 1도 높이기
+                    int coast = temperature > prevTemperature ? prevCoast : prevCoast + a;
+                    updateMinCoast(prevTemperature + 1, coast, now);
+                }
+
+                if (onboard[i] != 1 || isInRange(prevTemperature)) {
+                    // 온도 유지
+                    int coast = temperature == prevTemperature ? prevCoast : prevCoast + b;
+                    updateMinCoast(prevTemperature, coast, now);
+                }
+            }
+            mem.add(now);
         }
 
-        return dp(0, temperature);
+        return mem.get(onboard.length - 1).values()
+                .stream()
+                .mapToInt(i -> i)
+                .min()
+                .getAsInt();
     }
 
-    private int dp(int time, int inside) {
-        if (onboard[time] == 1 && (inside < minT || inside > maxT)) {
-            // 적정 온도 지키지 못해서 실패
-            minCoast.get(time).put(inside, -1);
-            return -1;
-        }
+    private boolean isInRange(int temperature) {
+        return min <= temperature && temperature <= max;
+    }
 
-        if (time == onboard.length - 1) {
-            // 시간 끝
-            return 0;
-        }
-
-        if (minCoast.get(time).containsKey(inside)) {
-            return minCoast.get(time).get(inside);
-        }
-
-        int answer = Integer.MAX_VALUE;
-
-        // 온도 1도 낮추는 경우
-        int coastDown = inside > outside ? 0 : coastDiff;
-        int coastAfterDown = dp(time + 1, inside - 1);
-        if (coastAfterDown != -1) {
-            answer = Math.min(coastDown + coastAfterDown, answer);
-        }
-
-        // 온도 1도 올리는 경우
-        int coastUp = inside < outside ? 0 : coastDiff;
-        int coastAfterUp = dp(time + 1, inside + 1);
-        if (coastAfterUp != -1) {
-            answer = Math.min(coastUp + coastAfterUp, answer);
-        }
-
-        // 온도 유지하는 경우
-        int coastMaintain = inside == outside ? 0 : coastSame;
-        int coastAfterMaintain = dp(time + 1, inside);
-        if (coastAfterMaintain != -1) {
-            answer = Math.min(coastMaintain + coastAfterMaintain, answer);
-        }
-
-        if (answer == Integer.MAX_VALUE) {
-            // 어떻게 해도 적정온도 못지켜서 실패
-            minCoast.get(time).put(inside, -1);
-            return -1;
-        }
-
-        minCoast.get(time).put(inside, answer);
-        return answer;
+    private void updateMinCoast(int temperature, int coast, Map<Integer, Integer> coasts) {
+        int min = Math.min(coast, coasts.getOrDefault(temperature, Integer.MAX_VALUE));
+        coasts.put(temperature, min);
     }
 }
